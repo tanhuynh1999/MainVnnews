80ef5a173dfff0d59564a05bcf17aaa456479316
tanhuynh1999 <54873015+tanhuynh1999@users.noreply.github.com>
2021-03-31 00:39:03 +0700

    Add project files.

---
+++ VnnewsNews/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VnnewsNews.Startup))]
+namespace VnnewsNews
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
