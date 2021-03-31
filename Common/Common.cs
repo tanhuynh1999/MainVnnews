242ac647c058a363591c8998f4a07d254d697562
tanhuynh1999 <54873015+tanhuynh1999@users.noreply.github.com>
2021-03-31 19:13:46 +0700

    PUSH

---
+++ Common/Common.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Common
+    {
+        public static int GROUP_THICH = 1;
+        public static int GROUP_LUUBAIVIET = 2;
+        public static int GROUP_NEWS_CATEGORY = 3;
+    }
+}
---
+++ VnnewsNews/Controllers/AdsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.EF;
+
+namespace VnnewsNews.Controllers
+{
+    public class AdsController : Controller
+    {
+        public ActionResult CreateAds()
+        {
+            return View();
+        }
+        // GET: Ads
+        [HttpPost]
+        public ActionResult PostAds()
+        {
+            return View();
+        }
+    }
+}
