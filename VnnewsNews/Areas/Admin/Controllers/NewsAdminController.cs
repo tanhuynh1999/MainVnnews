02ec4fbfa80495c53005f41b30c1289481f85e02
tanhuynh1999 <54873015+tanhuynh1999@users.noreply.github.com>
2021-03-31 16:47:43 +0700

    push

---
+++ VnnewsNews/Areas/Admin/Controllers/NewsAdminController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Model.EF;
+
+namespace VnnewsNews.Areas.Admin.Controllers
+{
+    public class NewsAdminController : Controller
+    {
+        private DBvnewsEntities db = new DBvnewsEntities();
+
+        // GET: Admin/NewsAdmin
+        public ActionResult Index()
+        {
+            var news = db.News.Include(n => n.User);
+            return View(news.ToList());
+        }
+
+        // GET: Admin/NewsAdmin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // GET: Admin/NewsAdmin/Create
+        public ActionResult Create()
+        {
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "user_name");
+            return View();
+        }
+
+        // POST: Admin/NewsAdmin/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "vnew_id,vnew_title,vnew_content,vnew_view,vnew_active,vnew_option,vnew_datecreate,vnew_dateupdate,user_id,vnews_des,vnew_img")] News news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.News.Add(news);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "user_name", news.user_id);
+            return View(news);
+        }
+
+        // GET: Admin/NewsAdmin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "user_name", news.user_id);
+            return View(news);
+        }
+
+        // POST: Admin/NewsAdmin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "vnew_id,vnew_title,vnew_content,vnew_view,vnew_active,vnew_option,vnew_datecreate,vnew_dateupdate,user_id,vnews_des,vnew_img")] News news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(news).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "user_name", news.user_id);
+            return View(news);
+        }
+
+        // GET: Admin/NewsAdmin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // POST: Admin/NewsAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            News news = db.News.Find(id);
+            db.News.Remove(news);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
